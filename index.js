e2a3178f136afaaf8ea8365b4153605bd83ae0df
SeungBin Kim <seungbin0508@gmail.com>
2021-11-25 14:42:46 +0900

    Install morgan and Add implement access logging

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import morgan from 'morgan'
+import fs from 'fs'
+import path from 'path'
 
 dotenv.config()
 
 const app = express()
+
+// Logger
+
+const logPath = path.join(path.resolve(), 'access.log')
+const accessLogStream = fs.createWriteStream(logPath, { flags: 'a' })
+
+app.use(morgan('combined', { stream: accessLogStream}))
+
+// Router
 
 app.get('/', (req, res, next) => {
 	res.send('Hello')
@@ -13,7 +25,6 @@ app.use((err, req, res, next) => {
 	console.log('Error!')
 	res.status(500).send('Error!')
 })
-
 
 app.listen(process.env.PORT, () => {
 	console.log(`Server listening at PORT ${process.env.PORT}`)
