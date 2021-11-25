122fa68bd126ec6428eb0d3f2045d7f43c4b7229
SeungBin Kim <seungbin0508@gmail.com>
2021-11-25 14:27:14 +0900

    Add default router and error handler

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ dotenv.config()
 
 const app = express()
 
+app.get('/', (req, res, next) => {
+	res.send('Hello')
+})
+
+app.use((err, req, res, next) => {
+	console.log('Error!')
+	res.status(500).send('Error!')
+})
+
+
 app.listen(process.env.PORT, () => {
 	console.log(`Server listening at PORT ${process.env.PORT}`)
 })
