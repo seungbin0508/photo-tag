d793c59e55896279b204e3fa28132d706718558f
SeungBin Kim <seungbin0508@gmail.com>
2021-11-26 12:28:08 +0900

    Update completion log with chalk

--- database/index.js
+++ database/index.js
@@ -1,5 +1,6 @@
 import { MongoClient } from 'mongodb'
 import dotenv from 'dotenv'
+import chalk from 'chalk'
 
 dotenv.config()
 const databaseName = process.env.NODE_ENV
@@ -9,5 +10,5 @@ const URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@develop
 const connection = await MongoClient.connect(URI)
 const db = connection.db(databaseName)
 
-console.log(`Database connected to ${databaseName}`)
+console.log(chalk.green(`MongoDB Driver connected to ${databaseName}`))
 export default db
