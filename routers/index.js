74a0cf7aadfcc8bcf06ad26baf79944b60a634ed
SeungBin Kim <seungbin0508@gmail.com>
2021-11-26 14:09:26 +0900

    Add routers for users

--- routers/index.js
+++ routers/index.js
@@ -2,4 +2,13 @@ import { Router } from 'express'
 
 const router = new Router()
 
+router.route('/users').
+	get().
+	post()
+
+router.route('/users/:userId').
+	get().
+	put().
+	delete()
+
 export default router
