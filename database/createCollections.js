01a68d96db70975bd7baae6a9c3b28610ab0bd95
SeungBin Kim <seungbin0508@gmail.com>
2021-11-26 13:17:22 +0900

    Add createCollections.js
    It deletes and sets up the database with collections and their validators and index.

---
+++ database/createCollections.js
@@ -0,0 +1,7 @@
+/**
+ * This file is to create default collections for a database.
+ * It also includes validators and indexes for collections.
+ */
+
+import db from './index.js'
+import chalk from 'chalk'
