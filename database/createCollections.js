49b3ec64c5125d8aafe25468c278d12a379c142a
SeungBin Kim <seungbin0508@gmail.com>
2021-11-26 13:21:20 +0900

    Implement dropping all collections before setting up the database

--- database/createCollections.js
+++ database/createCollections.js
@@ -9,31 +9,41 @@ import chalk from 'chalk'
 console.log(chalk.blue('Starting to create collections with validators and indexes.'))
 console.group()
 
+const collections = (await db.listCollections().toArray()).map(v => v.name)
+
+console.log(`Dropping all existing collections (if exist): ${collections.join(', ')}.`)
+
+for (const collection of collections) {
+	await db.collection(collection).drop()
+}
+
 /**
  * users collection.
  */
 
-console.log('Creating users collection...')
+	console.log('Creating users collection...')
+
 
 /**
  * images collection
  */
 
-console.log('Creating images collection...')
+	console.log('Creating images collection...')
+
 
 /**
  * comments collection
  */
 
-console.log('Creating comments collection...')
+	console.log('Creating comments collection...')
+
 
 /**
  * tags collections
  */
 
-console.log('Creating tags collection...')
+	console.log('Creating tags collection...')
 
-console.log(chalk.green(
-	`All collections are created. Created collections: .`))
+console.log(chalk.green(`All collections are created. Created collections: .`))
 console.groupEnd()
 
