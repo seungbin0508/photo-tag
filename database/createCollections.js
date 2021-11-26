64cfe2939b4d04ca65fc65d9c2cc61d19fdd44d5
SeungBin Kim <seungbin0508@gmail.com>
2021-11-26 13:19:52 +0900

    Add console logs to predefine the entire process

--- database/createCollections.js
+++ database/createCollections.js
@@ -5,3 +5,35 @@
 
 import db from './index.js'
 import chalk from 'chalk'
+
+console.log(chalk.blue('Starting to create collections with validators and indexes.'))
+console.group()
+
+/**
+ * users collection.
+ */
+
+console.log('Creating users collection...')
+
+/**
+ * images collection
+ */
+
+console.log('Creating images collection...')
+
+/**
+ * comments collection
+ */
+
+console.log('Creating comments collection...')
+
+/**
+ * tags collections
+ */
+
+console.log('Creating tags collection...')
+
+console.log(chalk.green(
+	`All collections are created. Created collections: .`))
+console.groupEnd()
+
